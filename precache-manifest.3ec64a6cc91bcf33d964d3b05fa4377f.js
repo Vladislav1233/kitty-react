3258831d39acf745fd5615aa7bb89fe33f8a61a7
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2019-07-14 14:19:15 +0400

    Updates

---
+++ precache-manifest.3ec64a6cc91bcf33d964d3b05fa4377f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68faaae7c38edfadacc17892d5d91505",
+    "url": "/kitty-react/index.html"
+  },
+  {
+    "revision": "5d8ad7178846840bc8f7",
+    "url": "/kitty-react/static/css/main.65121a60.chunk.css"
+  },
+  {
+    "revision": "0ba05ae42430c5ac1131",
+    "url": "/kitty-react/static/js/2.009169bb.chunk.js"
+  },
+  {
+    "revision": "5d8ad7178846840bc8f7",
+    "url": "/kitty-react/static/js/main.c575dfc5.chunk.js"
+  },
+  {
+    "revision": "f938748baac240563cd9",
+    "url": "/kitty-react/static/js/runtime~main.fd7c6ff8.js"
+  },
+  {
+    "revision": "6422e5995cb54f7357f76ace9ad1b951",
+    "url": "/kitty-react/static/media/cat-photo.6422e599.png"
+  },
+  {
+    "revision": "367c450a72ba9b5c0841ddd51889d1f7",
+    "url": "/kitty-react/static/media/exo-thin.367c450a.eot"
+  },
+  {
+    "revision": "4b0dc2ae3f6e1d0c206944efb2c99e7d",
+    "url": "/kitty-react/static/media/exo-thin.4b0dc2ae.woff2"
+  },
+  {
+    "revision": "dc192918d5c2185f96d9002d04f3d2e1",
+    "url": "/kitty-react/static/media/exo-thin.dc192918.woff"
+  },
+  {
+    "revision": "fc9b92b935a4d17f16439d14c2f88a37",
+    "url": "/kitty-react/static/media/exo-thin.fc9b92b9.ttf"
+  },
+  {
+    "revision": "15035d62b33c4a716ccc4e4927ef56b9",
+    "url": "/kitty-react/static/media/trebuchetms-bold.15035d62.woff2"
+  },
+  {
+    "revision": "3118ad8582125ba857c028771a9735b7",
+    "url": "/kitty-react/static/media/trebuchetms-bold.3118ad85.woff"
+  },
+  {
+    "revision": "5407c355f866b78377e3e928088d3173",
+    "url": "/kitty-react/static/media/trebuchetms-bold.5407c355.ttf"
+  },
+  {
+    "revision": "c4dc3d748644c2c4c635d0462359605a",
+    "url": "/kitty-react/static/media/trebuchetms-bold.c4dc3d74.eot"
+  },
+  {
+    "revision": "5e3ae7b1d7f404f422d7fbdc8fc8db31",
+    "url": "/kitty-react/static/media/trebuchetms.5e3ae7b1.woff"
+  },
+  {
+    "revision": "a89deb9576a7d390daf14a652f9ef892",
+    "url": "/kitty-react/static/media/trebuchetms.a89deb95.woff2"
+  },
+  {
+    "revision": "b58e9dfad8a6fdb76d33566f42cb0f9c",
+    "url": "/kitty-react/static/media/trebuchetms.b58e9dfa.eot"
+  },
+  {
+    "revision": "dda2997b8d235667f1d9eaf21ddf8c4b",
+    "url": "/kitty-react/static/media/trebuchetms.dda2997b.ttf"
+  }
+]);
